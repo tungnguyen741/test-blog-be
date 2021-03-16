66132784230141b838104362d5270b227fe874d0
Tùng Nguyễn <tung.nguyen21098@gmail.com>
2021-03-16 23:32:35 +0700

    ''

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -3,5 +3,5 @@ const postModel = require('../models/post.model')
 module.exports.post = async function(req, res){
     let posts = await postModel.find();
     console.log(posts)
-    res.json(posts)
+    return res.json(posts)
 }
