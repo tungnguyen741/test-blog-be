bf9dc2bb5d51a3bc86b122edd2b3c1b42ff75c39
Tùng Nguyễn <tung.nguyen21098@gmail.com>
2021-03-16 23:16:15 +0700

    ''

--- controller/create.controller.js
+++ controller/create.controller.js
@@ -1,8 +1,8 @@
 const userModel = require('../models/user.model')
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 module.exports.createUser = async function(req, res){
-    req.body.password = await bcrypt.hash(req.body.password, 10)
+    // req.body.password = await bcrypt.hash(req.body.password, 10)
     userModel.create(req.body);
     res.redirect("http://localhost:3000/signin")
 }
